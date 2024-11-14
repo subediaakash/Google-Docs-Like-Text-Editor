e3455f632f6420f042f295b717989a6a321f3e98
spartan-readytocode <7007966033abc@gmail.com>
2024-11-14 21:07:18 +0530

    feat : signin signup controllers

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,8 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
-const documentConnections = new Map();
-    
 app.listen(3000, () => {
   console.log("App listening on the port 3000");
 });
+
+
