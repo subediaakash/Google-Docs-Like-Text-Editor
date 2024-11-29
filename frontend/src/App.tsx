26eda9f89a216ebe59f7a1f9cecb37ca424683ac
spartan-readytocode <7007966033abc@gmail.com>
2024-11-29 22:52:37 +0530

    feat : http setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
-import { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-
 function App() {
-  const [value, setValue] = useState("hi");
-
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+  return <div>App</div>;
 }
 
 export default App;
