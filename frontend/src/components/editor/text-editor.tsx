a213c241caa2377ff36a5b6532b753adb218fa82
spartan-readytocode <7007966033abc@gmail.com>
2024-12-18 22:42:17 +0530

    feat : nothing extra change

--- frontend/src/components/editor/text-editor.tsx
+++ frontend/src/components/editor/text-editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
